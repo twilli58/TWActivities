0864e3254231da58297318cdcd2df43d52e9b631
twilli58 <twilli58@my.westga.edu>
2016-02-01 23:09:22 -0500

    Created new activity DisplayMessage for second tutorial

--- app/src/main/java/com/miko/tamikowilliamsactivities/MainActivity.java
+++ app/src/main/java/com/miko/tamikowilliamsactivities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.miko.tamikowilliamsactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.miko.tamikowilliamsactivities.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +29,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
