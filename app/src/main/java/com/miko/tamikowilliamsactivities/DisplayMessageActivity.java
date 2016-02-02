1c32d213815f886ac7e2aab48306702fe464c517
twilli58 <twilli58@my.westga.edu>
2016-02-01 23:37:47 -0500

    finished the tutorial and ran program to check its function

--- app/src/main/java/com/miko/tamikowilliamsactivities/DisplayMessageActivity.java
+++ app/src/main/java/com/miko/tamikowilliamsactivities/DisplayMessageActivity.java
@@ -1,11 +1,14 @@
 package com.miko.tamikowilliamsactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -25,6 +28,16 @@ public class DisplayMessageActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
     }
 
 }
